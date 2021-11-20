1644046b138c5ba836191f86cba279af35a7d522
Advaita Saha <adv11sphs@gmail.com>
2021-11-21 00:37:37 +0530

    crude implementation of favorites screen

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
+import '../providers/products.dart';
+import 'package:provider/provider.dart';
 import '../providers/product.dart';
-import '../widgets/product_item.dart';
+// import '../widgets/product_item.dart';
 import '../widgets/products_grid.dart';
+
+enum FilterOptions {
+  favorites,
+  all,
+}
 
 class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final productsContainer = Provider.of<Products>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: const Text('MyShop'),
+        actions: [
+          PopupMenuButton(
+            onSelected: (FilterOptions value) {
+              if (value == FilterOptions.favorites) {
+                productsContainer.showFavoritesOnly();
+              } else {
+                productsContainer.showAll();
+              }
+            },
+            itemBuilder: (_) => const [
+              PopupMenuItem(
+                child: Text('Only Favourites'),
+                value: FilterOptions.favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.all,
+              )
+            ],
+            icon: const Icon(Icons.more_vert),
+          ),
+        ],
       ),
       body: ProductsGrid(),
     );
